d4b82acb7f31f8bbf7ac58d5c6308d55cc65a886
Eduardo Carranza <88063305+EduardoCM96@users.noreply.github.com>
2024-10-16 13:33:59 -0600

    init proyect

---
+++ YouTubeClone/YouTubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTubeClone
+//
+//  Created by Eduardo Carranza maqueda on 16/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
